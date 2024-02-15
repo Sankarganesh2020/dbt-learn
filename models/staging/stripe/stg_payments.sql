82818f37683e8f060277493505ab1e41b09dfa28
sankarbyrna <sankarganesh@tripeak.tech>
2024-02-15 22:20:31 +0530

    first commit

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,12 @@
+select 
+    ID as payment_id,
+    ORDERID as order_id,
+    PAYMENTMETHOD as payment_method,
+    STATUS,
+
+    -- amount is stored in cents, convert it to dollars
+    AMOUNT / 100 as amount,
+    CREATED as created_at
+    
+    FROM
+        {{ source('stripe', 'stripe_payments')}} 
